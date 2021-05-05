29242623888f84469c519db5b5605b0fc978e25c
Vladimir Milojevic <vladamx.dev@gmail.com>
2021-05-05 21:05:36 +0200

    Add colors screen

---
+++ screens/ColorsScreen.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
+import { useAppSelector } from "../types";
+import { useNavigation } from "@react-navigation/native";
+import { NewColorNavigationProp } from "../components/AddColorButton";
+
+const ListEmpty = () => {
+  const navigation = useNavigation<NewColorNavigationProp>();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+      }}
+    >
+      <Text>There is no colors in your colors list.</Text>
+      <TouchableOpacity
+        style={{ marginTop: 10 }}
+        onPress={() => {
+          navigation.navigate("NewColor");
+        }}
+      >
+        <Text style={{ color: "blue" }}>Add color</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export const ColorsScreen = () => {
+  const favoriteColors = useAppSelector((store) => store.colorPicker.colors);
+  return (
+    <FlatList
+      data={favoriteColors}
+      keyExtractor={(_item, index) => {
+        return `color-${index}`;
+      }}
+      ListEmptyComponent={ListEmpty}
+      renderItem={({ item, index }) => (
+        <View key={index} style={[styles.item, { backgroundColor: item }]}>
+          <View style={[{ backgroundColor: "black" }]}>
+            <Text
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
+              {item}
+            </Text>
+          </View>
+        </View>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    margin: 20,
+    flex: 1,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
