206a80f8501e35473ba744cb3ae89d13124e75f6
Vladimir Milojevic <vladamx.dev@gmail.com>
2021-05-05 21:02:17 +0200

    Add color button

---
+++ components/AddColorButton.tsx
@@ -0,0 +1,25 @@
+import { TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import * as React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../types";
+import { StackNavigationProp } from "@react-navigation/stack";
+
+export type NewColorNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "NewColor"
+>;
+
+export const AddColorButton = () => {
+  const navigation = useNavigation<NewColorNavigationProp>();
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("NewColor");
+      }}
+      style={{ margin: 10 }}
+    >
+      <Feather name="plus" size={24} color="black" />
+    </TouchableOpacity>
+  );
+};
